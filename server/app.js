76c81a445e482beaa419df2fb0d60043fe97792c
Filip Jůza <filip@unchained.studio>
2019-11-04 19:09:05 +0100

    Setup basic routing

--- server/app.js
+++ server/app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const questionRouter = require('./routes/question');
 
 const app = express();
 
@@ -12,7 +11,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '..', 'client/build')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/question', questionRouter);
+
+// "Redirect" all get requests (except for the routes specified above) to React's entry point (index.html) to be handled by Reach router
+// It's important to specify this route as the very last one to prevent overriding all of the other routes
+app.get('*', (req, res) => res.sendFile(path.resolve('..', 'client', 'build', 'index.html')));
 
 module.exports = app;
--- server/routes/question.js
+++ server/routes/question.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Express' });
+    res.json({ title: 'Hello' });
 });
 
 module.exports = router;
